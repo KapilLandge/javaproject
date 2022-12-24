b31a12b85815edde2d3b28350266b5a3dd6c9dfc
KapilLandge <klandge8@gmail.com>
2022-12-24 17:34:59 +0530

    First arraylist demo code

---
+++ src/com/kapil/demo/ArrayListDemo.java
@@ -0,0 +1,18 @@
+package com.kapil.demo;
+
+import java.util.ArrayList;
+
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList<String> list=new ArrayList<String>();
+		list.add("Kapil");
+		list.add("Vaibhav");
+		list.add("Ashutosh");
+		System.out.println("ArayList is--"+list);
+	}
+
+}
